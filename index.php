0b2ca2a513714dfdad29f98018404cdc27e546d4
Matt Inkeles <mattinkeles37@gmail.com>
2020-11-07 14:53:08 -0500

    Added div tags "GET" and "POST" for the two different states (One is for grade entry and the other is for grade retrieval).

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
   <link rel="shortcut icon" href="">
 </head>
 
-<h1>Student Grades</h1>
+<button onclick="myFunction()">Toggle Mode</button>
+
+<div id="GET">
+<h1>Student Grade Retrieval</h1>
 <?php
 try {
     $conn = new PDO("sqlsrv:server = tcp:cloudcomputingclassdb.database.windows.net,1433; Database = CloudComputingClassDB", "mattink37", "a:U7wp_a");
@@ -35,8 +38,59 @@ echo "<form action=\"\">
             echo "<option value=" . $studentID . ">student " . $studentID . "</option>";
           }
 echo "</select></form>";
-?>
-<div id="txtHint">Student grades will be listed here</div>
+?><div id="txtHint">Student grades will be listed here</div></div>
+
+<div id="POST" style="display:none">
+<h1>Student Grade Entry</h1>
+<?php
+try {
+    $conn = new PDO("sqlsrv:server = tcp:cloudcomputingclassdb.database.windows.net,1433; Database = CloudComputingClassDB", "mattink37", "a:U7wp_a");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// // SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "mattink37", "pwd" => "a:U7wp_a", "Database" => "CloudComputingClassDB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:cloudcomputingclassdb.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+$tsql= "SELECT DISTINCT id FROM Grades";
+$getResults = sqlsrv_query($conn, $tsql);
+
+$data = array();
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))
+{
+  $data[] = $row;
+}
+
+echo "<form action=\"\">
+        <select name=\"students\" onchange=\"showStudent(this.value)\">
+          <option value=\"\">Select a student:</option>";
+          for ($i = 0; $i < count($data); $i++)
+          {
+            $studentID = implode(" ", $data[$i]);
+            echo "<option value=" . $studentID . ">student " . $studentID . "</option>";
+          }
+echo "</select></form>";
+?></div>
+
+
+<script>
+function myFunction() {
+  let get = document.getElementById("GET");
+  let post = document.getElementById("POST");
+
+  if (get.style.display === "none") {
+    get.style.display = "block";
+    post.style.display = "none";
+  } else {
+    post.style.display = "block";
+    get.style.display = "none";
+  }
+}
+</script>
 
 <script>
   function showStudent(str) {
