8229bb417172bcbe6cb8b960f9a85c915c89b3a7
Matt Inkeles <mattinkeles37@gmail.com>
2020-11-07 14:03:21 -0500

    inline php script now generates selection box with all the student id's from the database

--- index.php
+++ index.php
@@ -2,15 +2,39 @@
   <link rel="shortcut icon" href="">
 </head>
 
-<form action="">
-  <select name="students" onchange="showStudent(this.value)">
-    <option value="">Select a student:</option>
-    <option value="0">student 0</option>
-    <option value="1">student 1</option>
-    <option value="123">student 123</option>
-  </select>
-</form>
-<br>
+<?php
+try {
+    $conn = new PDO("sqlsrv:server = tcp:cloudcomputingclassdb.database.windows.net,1433; Database = CloudComputingClassDB", "mattink37", "a:U7wp_a");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// // SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "mattink37", "pwd" => "a:U7wp_a", "Database" => "CloudComputingClassDB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:cloudcomputingclassdb.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+$tsql= "SELECT DISTINCT id FROM Grades";
+$getResults = sqlsrv_query($conn, $tsql);
+
+$data = array();
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))
+{
+  $data[] = $row;
+}
+
+echo "<form action=\"\">
+        <select name=\"students\" onchange=\"showStudent(this.value)\">
+          <option value=\"\">Select a student:</option>";
+          for ($i = 0; $i < count($data); $i++)
+          {
+            $studentID = implode(" ", $data[$i]);
+            echo "<option value=" . $studentID . ">student " . $studentID . "</option>";
+          }
+echo "</select></form>";
+?>
 <div id="txtHint">Student grades will be listed here</div>
 
 <script>
